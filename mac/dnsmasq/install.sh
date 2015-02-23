1d4d33b27e1497408d73dbd807f39dfa65006657
Patryk Makowski <makowski.pat+git@gmail.com>
2015-02-23 14:05:16 +0100

    Install DNS masquerade

---
+++ mac/dnsmasq/install.sh
@@ -0,0 +1,10 @@
+# source: http://mallinson.ca/osx-web-development/
+
+brew install dnsmasq
+cd $(brew --prefix)
+mkdir etc
+echo 'address=/.dev/127.0.0.1' > etc/dnsmasq.conf
+sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons
+sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+sudo mkdir /etc/resolver
+sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/dev'
