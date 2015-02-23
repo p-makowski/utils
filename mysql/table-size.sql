739ce539646df63d41e5af4c9ce498246253301d
Patryk Makowski <makowski.pat+git@gmail.com>
2015-02-23 13:48:10 +0100

    Table size

---
+++ mysql/table-size.sql
@@ -0,0 +1,9 @@
+# source: http://dba.stackexchange.com/questions/1282/how-do-i-get-progress-for-type-db-sql-mysql/1307#1307 (RolandoMySQLDBA)
+
+SELECT CONCAT(db,'.',tb) table_name,CONCAT(ROUND(
+tbsz/POWER(1024,IF(pw<0,0,IF(pw>4,4,pw))),3),' ',
+SUBSTR(' KMGT',IF(pw<0,0,IF(pw>4,4,pw))+1,1)) table_size
+FROM (SELECT data_length+index_length tbsz,table_schema db,table_name tb
+FROM information_schema.tables WHERE engine='InnoDB') A,
+(SELECT 3 pw) B;
+
