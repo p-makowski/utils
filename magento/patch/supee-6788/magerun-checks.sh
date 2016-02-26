560996732b34c75174aa4f08d91885d669f5acc0
Patryk Makowski <makowski.pat+git@gmail.com>
2016-02-26 15:22:51 +0100

    vhost additiona config

--- magento/patch/supee-6788/magerun-checks.sh
+++ magento/patch/supee-6788/magerun-checks.sh
@@ -1,6 +1,13 @@
 LOG_FILE="supee-6788-magerun.log"
 
-n98-magerun.phar dev:template-vars >> $LOG_FILE
-n98-magerun.phar dev:possi >> $LOG_FILE
-n98-magerun.phar dev:old-admin-routing >> $LOG_FILE
-find . -type f -name 'register.phtml' -o -name 'resetforgottenpassword.phtml' | egrep -v "^(./vendor/vaimo/magento-enterprise/|./vendor/vaimo/magento/|./htdocs/)" >> $LOG_FILE
+echo -e "\nTEMPLATE VARS"
+n98-magerun.phar dev:template-vars
+
+echo -e "\nSQL INJECTIONS"
+n98-magerun.phar dev:possi
+
+echo -e "\nINCOMPATIBLE MODULES (ADMIN ROUTING)"
+n98-magerun.phar dev:old-admin-routing
+
+echo -e "\nTEMPLATES WITHOUT FORM KEY"
+find . -type f -name 'register.phtml' -o -name 'resetforgottenpassword.phtml' | egrep -v "^(./vendor/vaimo/magento-enterprise/|./vendor/vaimo/magento/|./htdocs/)"
