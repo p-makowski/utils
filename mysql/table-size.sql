cc630addbdbd9e7c8bd6db45f53d9193fe05c984
p-makowski <p-makowski@users.noreply.github.com>
2020-06-03 16:19:06 +0200

    Update table-size.sql

--- mysql/table-size.sql
+++ mysql/table-size.sql
@@ -1,19 +1,21 @@
 # source: http://dba.stackexchange.com/questions/1282/how-do-i-get-progress-for-type-db-sql-mysql/1307#1307 (RolandoMySQLDBA)
 
-SELECT CONCAT(db, '.', tb)                                              table_name,
+SELECT CONCAT(db, '.', tb) AS table_name,
        CONCAT(
-           ROUND(
-               tbsz / POWER(1024, IF(pw < 0, 0, IF(pw > 4, 4, pw))),
-               3
-           ),
-           ' ',
-            SUBSTR(' KMGT', IF(pw < 0, 0, IF(pw > 4, 4, pw)) + 1, 1)
-       ) table_size
+               ROUND(
+                           tbsz / POWER(1024, IF(pw < 0, 0, IF(pw > 4, 4, pw))),
+                           3
+                   ),
+               ' ',
+               SUBSTR(' KMGT', IF(pw < 0, 0, IF(pw > 4, 4, pw)) + 1, 1)
+           ) AS table_size
 FROM (
-    SELECT data_length + index_length tbsz, table_schema db, table_name tb
-      FROM information_schema.tables
-      WHERE engine = 'InnoDB'
-    ORDER BY tbsz ASC
-    ) A,
-    (SELECT 3 pw) B;
+         SELECT data_length + index_length tbsz, table_schema db, table_name tb
+         FROM information_schema.tables
+         WHERE engine = 'InnoDB'
+     ) AS A,
+     (SELECT 2 AS pw) AS B
+WHERE db = "{DB_NAME}"
+ORDER BY tbsz
+DESC LIMIT 50;
 
